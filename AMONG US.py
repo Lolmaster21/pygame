1c24a67d58dea585ae84b1ba96b8eabb4408e6e2
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-10-04 09:57:51 -0600

    Update AMONG US.py

--- AMONG US.py
+++ AMONG US.py
@@ -8,6 +8,39 @@ screen = pygame.display.set_mode((800,800))
 screen.fill((0,0,0))
 # font = pygame.freetype.Font("amatic.ttf", 80)
 
+#Boxie-------------------------------------------------------------------------
+class Box():
+    def __init__(self,position,color):
+        self.position = position
+        self.color = color
+        self.connected = False
+        self.clicked = False
+        self.hovering = False
+        
+    def BBcollision(xpos,ypos,x,y):
+        if x<xpos+100 and x>xpos and y>ypos and y<ypos+50:
+            return True
+        else:
+            False
+            
+        def isHovering(self, MouseX, Mousey):
+            if BBcollision(self.position[0],self.position[1],MouseX,Mousey)==True:
+                self.hovering = True
+            else:
+                self.hovering = False
+    
+
+    
+#Drawing class-----------------------------------------------
+    def draw():
+        pygame.draw.rect(screen,self.color, (self.position[0],self.position[1],100, 50))
+    
+        if self.hovering == True:
+            pygame.draw.rect(screen,(255,255,255), (self.position[0],self.position[1],100, 50),2)
+
+        
+    
+
 #colooorssss/ variables
 YELLOW = (250,250,0)
 RED = (200,0,0)
@@ -16,8 +49,10 @@ BLUE = (0,0,200)
 WHITE = (255,255,255)
 xpos = 0
 ypos = 0
-mousePos = (xpos, ypos) #variable mousePos stores TWO numbers
+#variable mousePos stores TWO numbers
 draw = False
+mouseDown = False
+mousePos = (0,0)
 
 
 numLines = 0 #variavle to keep track of number of lines so we only draw one at a time
@@ -27,15 +62,30 @@ StartBox = 0
 EndBox = 0
 
 #lists that hold positions and colors of wire boxes
-LeftPositions = [(0,100),(0,300),(0,500),(0,700)]
-RightPositions = [(750,100),(750,300),(750,500),(750,700)]
+LeftPosition = [(0,100),(0,300),(0,500),(0,700)]
+RightPosition = [(750,100),(750,300),(750,500),(750,700)]
+LeftColors = [RED, YELLOW, PINK, BLUE]
+RightColors = [RED, YELLOW, PINK, BLUE]
+random.shuffle(LeftColors)
+random.shuffle(RightColors)
+random.shuffle(LeftPosition)
+random.shuffle(RightPosition)
+
+#More boxesssss
+for i in range(4):
+    BoxList.append(Box(LeftPosition[i], LeftColors[i]))
+    BoxList.append(Box(RightPosition[i], RightColors[i]))
 
 
 
 #gameloop###################################################
+    
 while True:
 #event queue (bucket that holds stuff that happens in game and passes to one of the sections below)
     event = pygame.event.wait()
+    
+    
+    
 #update/timer section---------------------------------------    
     if mousePos[0] > 0 and mousePos[0] < 50 and mousePos[1] >750:
         color = RED
@@ -43,6 +93,8 @@ while True:
         color = BLUE
     #add other colors here!
 #input section----------------------------------------------
+   
+    
     if event.type == pygame.QUIT: #close game window
         break
 
@@ -54,24 +106,16 @@ while True:
 
     if event.type == pygame.MOUSEMOTION:
         mousePos = event.pos
+#physics section--------------------------------------------
+        for i in range(len(BoxList)):
+            BoxList[i].isHovering(mousePos[0],mousePos[1])
+
  
 #render section---------------------------------------------
-    if draw == True:
-        pygame.draw.circle(screen, color, (mousePos), 10) #player paintbrush
+    screen.fill((0,0,0))
     
-    #color changing rectangles at bottom of screen 
-    pygame.draw.rect(screen, RED, (LeftPositions[0][0],LeftPositions[0][1] ,50, 50))
-    pygame.draw.rect(screen, BLUE, (LeftPositions[1][0],LeftPositions[1][1] ,50, 50))
-    pygame.draw.rect(screen, YELLOW, (LeftPositions[2][0],LeftPositions[2][1] ,50, 50))
-    pygame.draw.rect(screen, PINK, (LeftPositions[3][0],LeftPositions[3][1] ,50, 50))
-    
-    pygame.draw.rect(screen, RED, (RightPositions[0][0],RightPositions[0][1] ,50, 50))
-    pygame.draw.rect(screen, BLUE, (RightPositions[1][0],RightPositions[1][1] ,50, 50))
-    pygame.draw.rect(screen, YELLOW, (RightPositions[2][0],RightPositions[2][1] ,50, 50))
-    pygame.draw.rect(screen, PINK, (RightPositions[3][0],RightPositions[3][1] ,50, 50))
-    
-
-    #more colors go here!
+    for i in range(len(BoxList)):
+        BoxList[i].draw()
         
     pygame.display.flip()
     
@@ -79,6 +123,7 @@ while True:
 #end game loop##############################################
 
 pygame.quit()
+
 
 
 
