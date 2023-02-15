aa7a9a924a904d0afb3f250aa831c2d9d016f302
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-02-15 09:59:30 -0700

    Update space invader.py

--- space invader.py
+++ space invader.py
@@ -15,14 +15,46 @@ UP = 2
 xpos = 430 #xpos of player
 ypos = 750 #ypos of player
 vx = 0 #x velocity of player
+timer = 0;
 
 keys = [False, False, False, False,False, False, False, False] #this list holds whether each key has been pressed
 #this list holds whether each key has been pressed
 
+class Alien:
+    def __init__(self,xpos,ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.isAlive = True
+        self.direction = 1
+    
+    def draw(self):
+        pygame.draw.rect(screen,(250,250,250),(self.xpos,self.ypos, 40,40))
+    
+    def move(self, time):
+        if timer %100==0:
+            self.ypos += 100
+            self.direction =-1
+            return 0
+        if time % 100 == 0:
+            self.xpos += 50*self.direction
+            
+        return time
+            
+            
+    
+        
+            
 
+bob = []
+for i in range(4):
+    for j in range(10):
+        bob.append(Alien(j*70+30, i*70+30))
 
 while not gameover: #GAME LOOP------------------------------------------------
     clock.tick(60) #FPS
+    timer += 1
+   
+
     
     #Input Section------------------------------------------------------------
     for event in pygame.event.get(): #quit game if x is pressed in top corner
@@ -71,12 +103,15 @@ while not gameover: #GAME LOOP------------------------------------------------
             
     screen.fill((0,0,0)) #wipe screen so it doesn't smear
     
+    
     #Player 1
     pygame.draw.rect(screen, (0, 250,0), (xpos, 750, 60, 20))
     pygame.draw.rect(screen, (0, 250,0), (xpos+5, 745, 50, 20))
     pygame.draw.rect(screen, (0, 250,0), (xpos+25, 736, 10, 20))
     pygame.draw.rect(screen, (0, 250,0), (xpos+28, 732, 4, 20))
-
+    
+    for i in range (len(bob)):
+        timer = bob[i].move(timer)
 
   
 
