f19b821ca5f42e3fe12bc86c43fc67cae910a810
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-02-27 09:37:46 -0700

    Update space invader.py

--- space invader.py
+++ space invader.py
@@ -1,4 +1,5 @@
 import pygame
+import random 
 pygame.init()  
 pygame.display.set_caption("Space Invaders")  # sets the window title
 screen = pygame.display.set_mode((1000, 800))  # creates game screen
@@ -9,16 +10,78 @@ gameover = False #variable to run our game loop
 #CONSTANTS
 LEFT=0
 RIGHT=1
-UP = 2
+SPACE = 2
+shoot = False
 
 #player variables
 xpos = 430 #xpos of player
 ypos = 750 #ypos of player
 vx = 0 #x velocity of player
-timer = 0;
+timer = 0
+lives = 3
 
 keys = [False, False, False, False,False, False, False, False] #this list holds whether each key has been pressed
 #this list holds whether each key has been pressed
+class Missles:
+    def __init__(self):
+        self.xpos = -10
+        self.ypos = -10
+        self.isAlive = False
+    
+    def draw(self):
+        if self.isAlive == True:
+            pygame.draw.rect(screen,(250,250,250), (self.xpos,self.ypos,10,10))
+
+    def move(self):
+        if self.isAlive == True:
+           self.ypos += 5
+           
+
+        if self.ypos == 800:
+            self.isAlive = False
+    
+
+rockets = []
+missle = Missles()
+for i in range(10):
+    rockets.append(Missles())
+
+
+
+class Wall:
+    def __init__(self,xpos,ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.isAlive = True
+        self.numHits = 0
+        self.c1 = 250
+        self.c3 = 20
+    def draw(self): 
+        if self.numHits < 3:
+            pygame.draw.rect(screen,(self.c1,self.c1,self.c3), (self.xpos,self.ypos,30,30))
+
+    
+    def collide(self, BulletX, BulletY):
+        if self.numHits < 3:
+            if BulletX > self.xpos:
+                if BulletX < self.xpos + 40:
+                    if BulletY < self.ypos + 40:
+                        if BulletY > self.ypos:
+                            print("Smacked wall")
+                            self.numHits += 1
+                            self.c1 -= 100
+                            self.c3 -= 10
+                            return False 
+        
+        return True 
+                            
+walls = []
+for k in range(4): #creates 4 sets
+    for i in range(2): #Handles rows 
+        for j in range(3): #handles collumns 
+            walls.append(Wall(j*30+200*k+150, i*30+600))
+
+
 
 class Bullet:
     def __init__(self,xpos,ypos):
@@ -33,13 +96,13 @@ class Bullet:
             self.isAlive = False
             self.xpos = xpos
             self.ypos = ypos
-    
-    
             
     def draw(self):
         pygame.draw.rect(screen,(250,250,250),(self.xpos,self.ypos,3,20))
+    
 
 bullet = Bullet(xpos+28,ypos)
+
         
 class Alien:
     def __init__(self,xpos,ypos):
@@ -49,8 +112,12 @@ class Alien:
         self.direction = 1
     
     def draw(self):
-        pygame.draw.rect(screen,(250,250,250),(self.xpos,self.ypos, 40,40))
-    
+        if self.isAlive == True:
+            pygame.draw.rect(screen,(250,250,250),(self.xpos,self.ypos, 40,40))
+        
+        if self.isAlive == False:
+            return False
+            
     def move(self,time):
         
         if time % 800 == 0:
@@ -59,10 +126,20 @@ class Alien:
             return 0
     
         if timer %100 == 0:
-            self.xpos += 30*self.direction
+            self.xpos += 35*self.direction
         
         return time
     
+    def collide(self, BulletX, BulletY):
+        if self.isAlive:
+            if BulletX > self.xpos:
+                if BulletX < self.xpos + 40:
+                    if BulletY < self.ypos + 40:
+                        if BulletY > self.ypos:
+                            print("HIT")
+                            self.isAlive = False
+                            return False
+        return True #otherwise  keep bullet alive
             
                
 
@@ -73,14 +150,11 @@ for i in range(4):
         
    
 
-while not gameover: #GAME LOOP------------------------------------------------
-    clock.tick(100) #FPS
+while(lives != 0): #GAME LOOP------------------------------------------------
+    clock.tick(60) #FPS
     timer += 1
     
     
-   
-
-
     
     #Input Section------------------------------------------------------------
     for event in pygame.event.get(): #quit game if x is pressed in top corner
@@ -90,6 +164,10 @@ while not gameover: #GAME LOOP------------------------------------------------
         if event.type == pygame.KEYDOWN: #keyboard input
             if event.key == pygame.K_LEFT:
                 keys[LEFT]=True
+
+            if event.key == pygame.K_SPACE:
+                keys[SPACE]=True
+                shoot = True
 
             elif event.key == pygame.K_RIGHT:
                 keys[RIGHT]=True
@@ -104,14 +182,16 @@ while not gameover: #GAME LOOP------------------------------------------------
             
             if event.key == pygame.K_RIGHT:
                 keys[RIGHT]=False
-  
+
+            if event.key == pygame.K_SPACE:
+                keys[SPACE]= False
+                shoot = False
 
     #LEFT MOVEMENT
     if keys[LEFT]==True:
         vx=-3
         direction = LEFT
-        
-  
+    
 
     #RIGHT MOVEMENT
     elif keys[RIGHT]==True:
@@ -121,17 +201,7 @@ while not gameover: #GAME LOOP------------------------------------------------
     #turn off velocity
     else:
         vx = 0
-        
-    #Shoot bullet
-    if shoot == True:
-        bullet.isAlive = True
-    
-    if bullet.isAlive == True:
-        bullet.move(xpos+28, ypos)
-    
-    else:
-        bullet.xpos = xpos +28
-        bullet.ypos = ypos 
+
 
     #update player position
     xpos+=vx
@@ -139,21 +209,95 @@ while not gameover: #GAME LOOP------------------------------------------------
     
     #Physics section--------------------------------------------------------------
     for i in range(len(bob)):
-        bob[i].move(timer)     
+        bob[i].move(timer)
+
+    for i in range(len(rockets)):
+        rockets[i].move()
+        
+    #shoot bullet
+    if shoot == True:
+        bullet.isAlive = True
+    if bullet.isAlive == True:
+        bullet.move(xpos+28, ypos)
+        if bullet.isAlive == True:
+            
+            for i in range (len(bob)):
+                bullet.isAlive = bob[i].collide(bullet.xpos, bullet.ypos)
+                if bullet.isAlive == False:
+                    break
+            
+    #Shoots wall
+        if bullet.isAlive == True:
+            for i in range(len(walls)):
+                bullet.isAlive = walls[i].collide(bullet.xpos, bullet.ypos)
+                if bullet.isAlive == False:
+                    break    
+    else:
+        bullet.xpos = xpos + 28
+        bullet.ypos = ypos 
+    
+    #Fire at will
+    lol = random.randrange(0,99)
+    if lol < 2:
+        print("bombs?")
+        pick = random.randrange(len(bob))
+        if bob[pick].isAlive == True:
+            for i in range(len(rockets)):
+                if rockets[i].isAlive == False:
+                    rockets[i].isAlive = True
+                    rockets[i].xpos = bob[pick].xpos+5
+                    rockets[i].ypos = bob[pick].ypos
+                    break 
+
+    for i in range(len(walls)):
+        for j in range (len(rockets)):
+            if rockets[j].isAlive == True:
+                if walls[i].collide(rockets[j].xpos, rockets[j].ypos) == False:
+                    rockets[j].isAlive = False
+                    break
+    
+    #player health collision 
+    for i in range(len(rockets)):
+        if rockets[i].isAlive:
+            if rockets[i].xpos>xpos:
+                if rockets[i].xpos < xpos +40:
+                    if rockets[i].ypos < ypos +40:
+                        if rockets[i].ypos > ypos:
+                            print(lives)
+                            lives -= 1
+                            xpos = 430
+                            ypos = 750
+                            if lives == 0:
+                                print("Game over T-T")
+                            
   
     # RENDER Section--------------------------------------------------------------------------------
             
     screen.fill((0,0,0)) #wipe screen so it doesn't smear
     
+    #Draws players bullet
+    missle.draw()
+    bullet.draw()
     
     #Player 1
     pygame.draw.rect(screen, (0, 250,0), (xpos, 750, 60, 20))
     pygame.draw.rect(screen, (0, 250,0), (xpos+5, 745, 50, 20))
     pygame.draw.rect(screen, (0, 250,0), (xpos+25, 736, 10, 20))
-    pygame.draw.rect(screen, (0, 250,0), (xpos+28, 732, 4, 20))
+    pygame.draw.rect(screen, (0, 250,0), (xpos+28, 732, 4, 20))    
     
+
+
+    #incoming walls
+    for i in range(len(walls)):
+        walls[i].draw()
+
+    #Enemy call function
     for i in range (len(bob)):
         bob[i].draw()
+
+    #Enemy call function
+    for i in range (len(rockets)):
+        rockets[i].draw()
 
   
 
@@ -161,3 +305,4 @@ while not gameover: #GAME LOOP------------------------------------------------
     
 #end game loop------------------------------------------------------------------------------
 pygame.quit()
+     
